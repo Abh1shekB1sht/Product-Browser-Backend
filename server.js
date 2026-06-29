d98be411deb2b4e50ad5dce086fa04e50f0d5298
Abhishek <singhbisht84484@gmail.com>
2026-06-28 22:27:14 -0700

    made some changes regarding deployement

--- server.js
+++ server.js
@@ -8,6 +8,14 @@ const productRoute = require("./routes/productRoute.js");
 
 const app = express();
 
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  if (req.method === "OPTIONS") return res.status(200).end();
+  next();
+});
+
 app.use(cors());
 app.use(express.json());
 
